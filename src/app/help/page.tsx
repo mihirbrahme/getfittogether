759c35df2c8d1c005ee1305dffdbda70b5723471
mihirb888 <mihir.brahme@sumasoft.net>
2025-12-29 19:56:40 +0530

    Fix: Resolve build error in help page

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -74,7 +74,7 @@ export default function HelpPage() {
                             </div>
                         </div>
                     </div>
-                </div3>
+                </div>
             </div>
 
             {/* How to Use */}
